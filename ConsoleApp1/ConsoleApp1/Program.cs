7a8be31007d0ddc71d27f3125eed1bad1123cfb6
Aaron Hansen <30009474@student.toiohomai.ac.nz>
2018-08-13 16:50:03 +1200

    You know what they say...

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string crazy = "";
+            Console.WriteLine("do I prefer dogs, cats, birds or bunnies?");
+            do
+            {
+                string CaseSwitch = Console.ReadLine();
+
+
+                switch (CaseSwitch)
+                {
+                    case "birds":
+                        Console.WriteLine("I do like birds! Did you know they are closely related to dinosaurs? Crazy right.");
+                        crazy = "yeah";
+                        break;
+                    case "dogs":
+                    case "cats":
+                    case "bunnies":
+                        Console.WriteLine("I mean, they're cool, but my favourite is slightly less mammal");
+                        break;
+                    default:
+                        Console.WriteLine("psst. hey. I gave you options for a reason.");
+                        break;
+                }
+            } while (crazy != "yeah");
+            Console.ReadLine();
+            Console.WriteLine("anyway... Peace!");
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+    }
+
